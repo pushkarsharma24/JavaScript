39b323e9f1b2754d7c6f1f2c33cbf703aca090d4
Pushkar Kr Sharma <pushkarkumarsharma2401@gmail.com>
2024-12-03 15:32:48 +0530

    2nd Commit

---
+++ Lecture1/Const Let Var.js
@@ -0,0 +1,3 @@
+const accountNo = 1234
+let accountEmail = "pushkar@gmail.com"
+var accoountHolder = "Pushkar"
---
+++ Lecture2/Datatypes and ECMA.js
@@ -0,0 +1,17 @@
+/*
+In javascript their are 8 basic types of DataTypes
+-number
+-bigInt
+-string
+-boolean
+-null => null is also a standalone value
+-undefiend
+-symbol => used to define uniqueness mostly used in react components and figma
+
+Non-Primitive
+- object => in coming days we are gonna study a lot about this
+
+*/
+//Interview question//
+console.log(typeof undefined) = undefined
+console.log(typeof null) = object
